00991555f1fedc25a9193ddd1a1e545c7f8e4b3a
leejeongjun <backbone94@naver.com>
2025-05-29 20:39:32 +0900

    chore: 공지, 1:1 문의 Mock 데이터 따로 분리

--- src/components/NoticeDetailPage.tsx
+++ src/components/NoticeDetailPage.tsx
@@ -1,26 +1,6 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import BottomBar from './BottomBar';
-
-const notices = [
-  {
-    id: 1,
-    title: '[필독] 셔틀버스 이용 안내',
-    date: '2025.05.15 08:48',
-    content: '셔틀버스 이용 시 QR코드 스캔은 필수입니다. 출퇴근 시간에 맞춰 이용해 주세요. QR코드 미지참 시 탑승이 제한될 수 있으니, 반드시 사전에 준비해 주시기 바랍니다. 또한, 탑승 시 마스크 착용을 권장합니다.'
-  },
-  {
-    id: 2,
-    title: '[공지] 5월 연휴 셔틀 미운행 안내',
-    date: '2025.05.01 09:40',
-    content: '5월 5일 어린이날은 법정 공휴일로 셔틀버스가 운행되지 않습니다. 연휴 기간 동안 셔틀 이용에 착오 없으시길 바라며, 자세한 일정은 추후 별도 안내드릴 예정입니다.'
-  },
-  {
-    id: 3,
-    title: '[안내] 앱 업데이트 안내',
-    date: '2025.04.20 09:30',
-    content: '슬기로운 아이티센 생활 앱이 업데이트 되었습니다. 최신 버전으로 업데이트 해 주세요. 이번 업데이트에서는 사용자 인터페이스 개선과 버그 수정, 그리고 새로운 기능이 추가되었습니다. 자세한 내용은 공지사항을 참고해 주세요.'
-  },
-];
+import notices from './noticesMock';
 
 export default function NoticeDetailPage() {
   const { id } = useParams();
