4c86bb87bb5c3a9a941615440fb00c46511ab505
youjiyeon <jiyeonyou0416@naver.com>
2025-06-03 11:12:24 +0900

    chore: 프로젝트 초기 설정

---
+++ seurasaeng 2/src/main/java/onehajo/seurasaeng/SeurasaengApplication.java
@@ -0,0 +1,13 @@
+package onehajo.seurasaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeurasaengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeurasaengApplication.class, args);
+    }
+
+}
---
+++ seurasaeng 2/src/test/java/onehajo/seurasaeng/SeurasaengApplicationTests.java
@@ -0,0 +1,13 @@
+package onehajo.seurasaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeurasaengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
