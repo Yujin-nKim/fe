38ea7e0df75e95435146c1dcfa3811a15f8a102e
leejeongjun <backbone94@naver.com>
2025-06-05 13:03:33 +0900

    fix: 실시간 셔틀 확인 페이지 URL 수정

--- seurasaeng_fe/src/components/MainPage.tsx
+++ seurasaeng_fe/src/components/MainPage.tsx
@@ -160,7 +160,7 @@ export default function MainPage() {
             <span className="text-xs mb-2 text-[#5382E0] w-36 text-left">셔틀에서 무엇을 잃어버리셨나요?</span>
             <span className="ml-auto mt-auto"><img src="/lost-items.png" alt="1:1 문의" className="w-7 h-7" /></span>
           </button>
-          <button className="col-span-1 bg-[#DEE9FF] rounded-xl p-4 flex flex-col items-start shadow-sm min-h-[90px]" onClick={() => navigate('/realtime')}>
+          <button className="col-span-1 bg-[#DEE9FF] rounded-xl p-4 flex flex-col items-start shadow-sm min-h-[90px]" onClick={() => navigate('/realtime-shuttle')}>
             <span className="font-bold text-base text-[#5382E0]">실시간 셔틀 확인</span>
             <span className="text-xs mb-2 text-[#5382E0] w-36 text-left">실시간으로 셔틀 위치를 확인하세요.</span>
             <span className="ml-auto mt-auto"><img src="/shuttle.png" alt="실시간 셔틀" className="w-7 h-7" /></span>
